1d319cffeb4881eaa970ee27b2f8e08b37c51c90
ktrfil3 <82050528+ktrfil3@users.noreply.github.com>
2025-12-01 16:40:39 -0400

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
         <section className="hero">
           <div className="hero-img">
             <video 
-              src="/home/C1079.mp4" 
+              src="/home/C0996.mp4" 
               autoPlay 
               loop 
               muted 
